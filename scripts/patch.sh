ee07d4a33119fd45107ddd2994ec5f0478927017
darkyzhou <me@zqy.io>
2025-03-13 21:53:14 +0800

    feat: electron 34.3.2

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -6,15 +6,15 @@ ROOT_PATH=${ROOT_PATH:-"/home/builduser/buildroot"}
 REPO_PATH=${REPO_PATH:-"/home/builduser/electron-loong64"}
 DEPOT_PATH=${DEPOT_PATH:-"/home/builduser/.electron_build_tools/third_party/depot_tools"}
 
-pushd "$ROOT_PATH"
+pushd "$ROOT_PATH/src/electron"
 
-git -C src/electron clean -fd
-git -C src/electron reset --hard HEAD
-git -C src/electron apply --reject "$REPO_PATH"/patches/chromium.patch
-git -C src/electron apply --reject "$REPO_PATH"/patches/electron.patch
-
-# Workaround for a strange Python error: "Cannot call rmtree on a symbolic link"
-git -C "$DEPOT_PATH" reset --hard HEAD
-git -C "$DEPOT_PATH" apply --reject "$REPO_PATH"/patches/depot.patch
+git clean -fd && git reset --hard HEAD
+git apply --reject "$REPO_PATH"/patches/chromium.patch
+git add .
+git commit -m "chromium.patch" --no-verify
+git apply --reject "$REPO_PATH"/patches/electron.patch
+git add .
+git commit -m "electron.patch" --no-verify
 
 popd
+
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -11,6 +11,7 @@ pushd "$ROOT_PATH"
 git -C "$DEPOT_PATH" reset --hard HEAD
 
 if ! npx e sync --three-way -f; then
+    # Workaround for a strange Python error: "Cannot call rmtree on a symbolic link"
     git -C "$DEPOT_PATH" apply "$REPO_PATH/patches/depot.patch"
     npx e sync --three-way -f
 fi
